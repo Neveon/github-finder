56e58a83e1f1b5a661c4143f6fe1d9006ef290d3
Neil T <neiltz711@gmail.com>
2019-06-09 18:02:23 -0400

    Context update

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,19 +1,20 @@
 import React, { Fragment, useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Repos from '../repos/Repos';
 import GithubContext from '../../context/github/githubContext';
 
-const User = ({ getUserRepos, repos, match }) => {
+const User = ({ match }) => {
   const githubContext = useContext(GithubContext);
 
-  const { getUser, loading, user } = githubContext;
+  const { getUser, loading, user, repos, getUserRepos } = githubContext;
 
   //Equivalent to componentDidMount
   useEffect(() => {
     getUser(match.params.login); // :login param
     getUserRepos(match.params.login);
+    // The below removes the useEffect warning.
+    // eslint-disable-next-line
   }, []);
 
   const {
@@ -105,11 +106,6 @@ const User = ({ getUserRepos, repos, match }) => {
       <Repos repos={repos} />
     </Fragment>
   );
-};
-
-User.propTypes = {
-  repos: PropTypes.array.isRequired,
-  getUserRepos: PropTypes.func.isRequired
 };
 
 export default User;
